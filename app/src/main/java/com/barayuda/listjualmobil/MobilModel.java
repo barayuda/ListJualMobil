55cf45e8570c02e124a48688fed6aa83ec7a3b4d
Barayuda Gautama <barayuda@Barayudas-MacBook-Pro.local>
2016-10-27 16:08:34 +0800

    Initial commit

---
+++ app/src/main/java/com/barayuda/listjualmobil/MobilModel.java
@@ -0,0 +1,43 @@
+package com.barayuda.listjualmobil;
+
+import java.io.Serializable;
+
+/**
+ * Created by barayuda on 10/27/16.
+ */
+
+public class MobilModel implements Serializable {
+    String title, image, harga, lokasi;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+}
